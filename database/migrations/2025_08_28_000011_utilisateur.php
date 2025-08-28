73bfbeb3867279e8a765d19ad29089680c49b617
GuiMusa <g.musaraganyi@gmail.com>
2025-08-28 09:54:32 -0400

    Corriger_Migration

---
+++ database/migrations/2025_08_28_000011_utilisateur.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('utilisateurs', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('date');
+            $table->string('nom')->unique();
+            $table->string('pseudo');
+            $table->string('photo_de_profil')->nullable();
+            $table->string('mail')->unique();
+            $table->string('mdp');
+            $table->enum('statue', ['actif', 'inactif', 'banni', 'modérateur'])->default('actif');
+            $table->foreignId('ID_article')->nullable()->constrained('articles')->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('utilisateurs');
+    }
+};
